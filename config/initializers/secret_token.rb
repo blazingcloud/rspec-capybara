6faca7feb25d20b060942ce708d04b2f345d427d
Rachel <rachel.marie.myers@gmail.com>
2010-12-17 13:54:10 -0800

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecCapybara::Application.config.secret_token = 'c1ae71cf76bb1149f1c7af5bff1890c4d401b6918445364e55c3f03021e2cab740982a5d137cfb1b01bb20e678cddf17fda572ad7943079fefe94e5ae00fef3d'
