b8cbf1d1dabe54f6ac90b20ec2093819e771b11a
Sarah Allen <sarah@ultrasaurus.com>
2010-12-18 12:02:05 -0800

    specs pass, removed unused spec/support config

--- spec/support/integration/integration_example_group.rb
+++ spec/support/integration/integration_example_group.rb
@@ -5,28 +5,16 @@ require 'capybara/dsl'
 module RSpec::Rails
   module IntegrationExampleGroup
     include ActionDispatch::Integration::Runner
-    #     include RSpec::Rails::TestUnitAssertionAdapter
-     #   include ActionDispatch::Assertions
     include Capybara
-    #include RSpec::Matchers
-    # 
-        def app
-            ::Rails.application
-        end
-    # 
-    #       def last_response
-    #         page
-    #       end
-    #     end
-    # 
-    #     included do
-    #       before do
-    #         @router = ::Rails.application.routes
-    #       end
-    # 
-        RSpec.configure do |c|
-          c.include self, :example_group => { :file_path => /\bspec\/integration\// }
-        end
+
+    def app
+        ::Rails.application
+    end
+
+    RSpec.configure do |c|
+      c.include self, :example_group => { :file_path => /\bspec\/integration\// }
+    end
+
   end
 end
 
